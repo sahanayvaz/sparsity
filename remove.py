e31af1f53eae455ccf3f37e2163065ea12f426f6
yvz <yvz@student-net-vpn-4076.ethz.ch>
2019-12-20 04:46:54 +0200

    add print to pass

--- remove.py
+++ remove.py
@@ -15,10 +15,6 @@ for l in list_dir:
 		save_dir_l = os.path.join(save_dir, l)
 		os.makedirs(save_dir_l, exist_ok=True)
 		if not '.npz' in j:
-			try:
-				file_path = os.path.join(full_l, j)
-				shutil.move(file_path, save_dir_l)
-			except:
-				print('passing {}'.format(j))
-				pass
+			file_path = os.path.join(full_l, j)
+			shutil.move(file_path, save_dir_l)
 print('done')
