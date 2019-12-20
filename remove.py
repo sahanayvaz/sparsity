65bfa5ca4a4d1b2758b6101d3c3b6f1c1036d0c2
yvz <yvz@student-net-vpn-4076.ethz.ch>
2019-12-20 04:06:31 +0200

    added remove.py to move saved models to save_dir

---
+++ remove.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+
+log_dir = '/cluster/sayvaz/sparsity/log_dir'
+save_dir = '/cluster/sayvaz/sparsity/save_dir'
+os.makedirs(save_dir, exist_ok=True)
+
+list_dir = os.listdir(log_dir)
+
+for l in list_dir:
+	full_l = os.path.join(log_dir, l)
+	l_dir = os.listdir(full_l)
+	for j in l_dir:
+		if not '.npz' in j:
+			file_path = os.path.join(full_l, j)
+			shutil.move(file_path, save_dir)
+print('done')
